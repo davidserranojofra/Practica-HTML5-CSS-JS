a42500a98485d493a97cd96f2a10d100d7fc6a96
David Serrano Jofra <davidserranojofra@gmail.com>
2017-07-06 07:55:03 +0200

    Empiezo con el Js y ya inserto ejercicio input Otro

---
+++ js/javascript.js
@@ -0,0 +1,39 @@
+// Input radio--> al seleccionar "Otro" se añade input
+
+var seleccionarInput = document.getElementById('ckbox');
+var otro = document.getElementById("otro");
+
+var creoInput = document.createElement("input");
+creoInput.setAttribute("type", "text");
+creoInput.setAttribute("name", "inputotro");
+creoInput.setAttribute("class", "inputotro");
+creoInput.setAttribute("id", "inputotro");
+creoInput.setAttribute("placeholder", "De que manera fue...");
+
+seleccionarInput.addEventListener("click", function (event) {
+
+    if (seleccionarInput.children[3].checked === true) {
+
+        otro.appendChild(creoInput);
+
+    } else if (otro.children[0]) {
+
+        otro.removeChild(creoInput); 
+
+    }
+});
+
+//Textarea --> contador de 150 pabras
+
+var contador;
+var textarea = document.getElementById("comentario");
+var insertarContador = document.getElementById("contadorPalabras");
+
+textarea.addEventListener("keypress", function(event) {
+
+    contador = textarea.value.split(" ");
+
+    console.log(contador.length);
+});
+
+
