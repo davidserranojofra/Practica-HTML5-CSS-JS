bdcbe73ff8bc052d25d7d561cdaa8d7d19a36b21
David Serrano Jofra <davidserranojofra@gmail.com>
2017-07-09 03:29:51 +0200

    añado el control spy para cambiar automaticamente el .active con el scroll

--- js/javascript.js
+++ js/javascript.js
@@ -21,7 +21,6 @@ seleccionarInput.addEventListener("click", function (event) {
 });
 
 
-
 // Textarea --> contador de 150 pabras
 
 var contador = 0;
@@ -52,6 +51,10 @@ for (var i = 0; i < elementosMenu.length; i++) {
         
         var seccionAIr = this.getElementsByTagName("a")[0].href.split("#");
 
+        borrarClaseActiva();
+        this.classList.add("active");
+        
+
         if (seccionAIr.length === 2) {
             event.preventDefault();
             var irA = seccionAIr[seccionAIr.length - 1];
@@ -76,8 +79,6 @@ function cogerIdParaScroll(id) {
 function hacerScroll(elemento) {
     
     var salto = elemento.getBoundingClientRect().top * 0.3;
-    console.log(salto);
-
     document.body.scrollTop += salto;
 
     if (!elemento.ultimoSalto || elemento.ultimoSalto > Math.abs(salto)) {
@@ -89,4 +90,97 @@ function hacerScroll(elemento) {
     } else {
         elemento.ultimoSalto = null;
     }
+}
+
+function borrarClaseActiva() {
+    for (var i = 0; i < elementosMenu.length; i++ ) {
+        elementosMenu[i].classList.remove("active");
+    }
+}
+
+
+//espia posicion para cambiar .active automaticamente
+
+var acumulaDesplazamiento = function(elemento) {
+    var top = 0;
+    console.log(elemento);
+    do {
+        top += elemento.offsetTop || 0;
+        elemento = elemento.offsetParent;
+        console.log(elemento);
+    } while (elemento);
+
+    return top;
+}
+
+var dondeEstaQuiensoy = acumulaDesplazamiento(document.getElementById("quiensoy")) - 50;
+var dondeEstaEstudios = acumulaDesplazamiento(document.getElementById("estudios")) - 50;
+var dondeEstaExperiencia = acumulaDesplazamiento(document.getElementById("experiencia")) - 50;
+var dondeEstaSobreMi = acumulaDesplazamiento(document.getElementById("sobre-mi")) - 50;
+var dondeEstaContacto = acumulaDesplazamiento(document.getElementById("contacto")) - 50;
+var menuNavegacion = document.getElementsByClassName("menu")[0];
+
+window.addEventListener("scroll", cambiarMenu);
+
+var previo;
+
+function cambiarMenu(event) {
+
+    var desplazamientoPagina = window.pageYOffset;
+
+    if (desplazamientoPagina >= 0 && desplazamientoPagina < dondeEstaQuiensoy) {
+        if (!previo || previo !== 1) {
+            previo = 1;
+        } else if (previo === 1){
+            return false;
+        }
+
+        borrarClaseActiva();
+        document.querySelector("a[href='#']").parentNode.classList.add("active");
+    } else if (desplazamientoPagina >= dondeEstaQuiensoy && desplazamientoPagina < dondeEstaEstudios) {
+        if (!previo || previo !== 2) {
+            previo = 2;
+        } else if (previo === 2){
+            return false;
+        }
+
+        borrarClaseActiva();
+        document.querySelector("a[href$='quiensoy']").parentNode.classList.add("active");
+    } else if (desplazamientoPagina >= dondeEstaEstudios && desplazamientoPagina < dondeEstaExperiencia) {
+        if (!previo || previo !== 3) {
+            previo = 3;
+        } else if (previo === 3){
+            return false;
+        }
+
+        borrarClaseActiva();
+        document.querySelector("a[href$='estudios']").parentNode.classList.add("active");  
+    } else if (desplazamientoPagina >= dondeEstaExperiencia && desplazamientoPagina < dondeEstaSobreMi) {
+        if (!previo || previo !== 4) {
+            previo = 4;
+        } else if (previo === 4){
+            return false;
+        }
+
+        borrarClaseActiva();
+        document.querySelector("a[href$='experiencia']").parentNode.classList.add("active");
+    } else if (desplazamientoPagina >= dondeEstaSobreMi && desplazamientoPagina < dondeEstaContacto) {
+        if (!previo || previo !== 5) {
+            previo = 5;
+        } else if (previo === 5){
+            return false;
+        }
+
+        borrarClaseActiva();
+        document.querySelector("a[href$='sobre-mi']").parentNode.classList.add("active");
+    } else {
+        if (!previo || previo !== 6) {
+            previo = 6;
+        } else if (previo === 6){
+            return false;
+        }
+
+        borrarClaseActiva();
+        document.querySelector("a[href$='contacto']").parentNode.classList.add("active");
+    } 
 }
